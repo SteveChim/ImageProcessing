f39c64417f9552b4952f6ceba6c998e0327c7463
SteveChim <steve_chim@yahoo.com.hk>
2018-10-02 16:03:24 +0800

    Add files via upload

---
+++ ImageProcessing/frmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ImageProcessing
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibFiltered = new Emgu.CV.UI.ImageBox();
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.tlpInner = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lblChosenFile = new System.Windows.Forms.Label();
+            this.tlpOuter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibFiltered)).BeginInit();
+            this.tlpInner.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpOuter
+            // 
+            this.tlpOuter.ColumnCount = 2;
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.Controls.Add(this.ibOriginal, 0, 1);
+            this.tlpOuter.Controls.Add(this.ibFiltered, 1, 1);
+            this.tlpOuter.Controls.Add(this.tlpInner, 0, 0);
+            this.tlpOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpOuter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tlpOuter.Location = new System.Drawing.Point(0, 0);
+            this.tlpOuter.Name = "tlpOuter";
+            this.tlpOuter.RowCount = 2;
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpOuter.Size = new System.Drawing.Size(924, 567);
+            this.tlpOuter.TabIndex = 0;
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibOriginal.Enabled = false;
+            this.ibOriginal.Location = new System.Drawing.Point(3, 50);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(456, 514);
+            this.ibOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibFiltered
+            // 
+            this.ibFiltered.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibFiltered.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibFiltered.Enabled = false;
+            this.ibFiltered.Location = new System.Drawing.Point(465, 50);
+            this.ibFiltered.Name = "ibFiltered";
+            this.ibFiltered.Size = new System.Drawing.Size(456, 514);
+            this.ibFiltered.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibFiltered.TabIndex = 2;
+            this.ibFiltered.TabStop = false;
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.FileName = "openFileDialog1";
+            // 
+            // tlpInner
+            // 
+            this.tlpInner.AutoSize = true;
+            this.tlpInner.ColumnCount = 2;
+            this.tlpOuter.SetColumnSpan(this.tlpInner, 2);
+            this.tlpInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpInner.Controls.Add(this.btnOpenFile, 0, 0);
+            this.tlpInner.Controls.Add(this.lblChosenFile, 1, 0);
+            this.tlpInner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpInner.Location = new System.Drawing.Point(3, 3);
+            this.tlpInner.Name = "tlpInner";
+            this.tlpInner.RowCount = 1;
+            this.tlpInner.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpInner.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpInner.Size = new System.Drawing.Size(918, 41);
+            this.tlpInner.TabIndex = 3;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.AutoSize = true;
+            this.btnOpenFile.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(3, 3);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(107, 35);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // lblChosenFile
+            // 
+            this.lblChosenFile.AutoSize = true;
+            this.lblChosenFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblChosenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChosenFile.Location = new System.Drawing.Point(116, 0);
+            this.lblChosenFile.Name = "lblChosenFile";
+            this.lblChosenFile.Size = new System.Drawing.Size(799, 41);
+            this.lblChosenFile.TabIndex = 1;
+            this.lblChosenFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 567);
+            this.Controls.Add(this.tlpOuter);
+            this.Name = "frmMain";
+            this.Text = "ImageProcessing";
+            this.tlpOuter.ResumeLayout(false);
+            this.tlpOuter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibFiltered)).EndInit();
+            this.tlpInner.ResumeLayout(false);
+            this.tlpInner.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpOuter;
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibFiltered;
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private System.Windows.Forms.TableLayoutPanel tlpInner;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label lblChosenFile;
+    }
+}
+
---
+++ ImageProcessing/frmMain.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;                  //
+using Emgu.CV.CvEnum;           // usual Emgu Cv imports
+using Emgu.CV.Structure;        //
+using Emgu.CV.UI;               //
+
+
+namespace ImageProcessing
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            DialogResult drChosenFile;
+
+            drChosenFile = ofdOpenFile.ShowDialog();
+
+            if (drChosenFile != DialogResult.OK || ofdOpenFile.FileName == "")
+            {
+                lblChosenFile.Text = "file not chosen";
+                return;
+            }
+
+            Mat inputFormColor;
+
+            try
+            {
+                inputFormColor = new Mat(ofdOpenFile.FileName, ImreadModes.Color);
+            }
+            catch (Exception ex)
+            {
+                lblChosenFile.Text = "unable to open image, error: " + ex.Message;
+                return;
+            }
+
+            if (inputFormColor == null)
+            {
+                lblChosenFile.Text = "unable to open image";
+                return;
+            }
+
+            CvInvoke.Resize(inputFormColor, inputFormColor, new Size(0, 0), 0.25, 0.25, Inter.Cubic);
+
+            Mat inputFormGray = new Mat(inputFormColor.Size, DepthType.Cv8U, 1);
+            Mat inputFormGrayFiltered = new Mat(inputFormColor.Size, DepthType.Cv8U, 1);
+            Mat inputFormComplement = new Mat(inputFormColor.Size, DepthType.Cv8U, 1);
+
+            inputFormComplement.SetTo(new MCvScalar(255));
+
+            CvInvoke.CvtColor(inputFormColor, inputFormGray, ColorConversion.Bgr2Gray);
+
+            CvInvoke.BilateralFilter(inputFormGray, inputFormGrayFiltered, 9, 80.0, 80.0);
+
+            CvInvoke.AdaptiveThreshold(inputFormGrayFiltered, inputFormGrayFiltered, 255, AdaptiveThresholdType.GaussianC, ThresholdType.Binary, 11, 2);
+
+            CvInvoke.Subtract(inputFormComplement, inputFormGrayFiltered, inputFormComplement);
+
+            Size inputFormSize = new Size(450, 650);
+
+            Point inputFormCenter = new Point(inputFormGray.Size.Width / 2, inputFormGray.Size.Height / 2);
+
+            CvInvoke.GetRectSubPix(inputFormComplement, inputFormSize, inputFormCenter, inputFormComplement);
+
+            Size sampleSize = new Size(45, 65);
+
+            int[,] histogram = new int[10,10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    int Histogram = 0;
+                    Mat sampleForm = new Mat(sampleSize, DepthType.Cv8U, 1);
+                    Point sampleSizeCenter = new Point(45 * i + 22, 65 * j + 32);
+                    CvInvoke.GetRectSubPix(inputFormComplement, sampleSize, sampleSizeCenter, sampleForm);
+                    for (int height = 0; height < sampleForm.Rows; height++)
+                        for (int width = 0; width < sampleForm.Cols; width++)
+                        {
+                            if (Convert.ToInt16(sampleForm.GetData(height, width)[0]) != 0)
+                            {
+                                Histogram++;
+                            }
+                        }
+                    histogram[i, j] = Histogram;
+                }
+            }
+
+            string fileName = System.IO.Path.GetFileNameWithoutExtension(ofdOpenFile.FileName);
+            string arrayFileName = "C:\\CsharpTemp\\" +  fileName + "_Array.txt";
+            using (StreamWriter sw = new StreamWriter(arrayFileName))
+            {
+                //sw.WriteLine("HistogramArray");
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        sw.Write(histogram[j, i] + " ");
+                    }
+                    sw.WriteLine("");
+                }
+
+                sw.Flush();
+                sw.Close();
+            }
+            
+
+
+            ibOriginal.Image = inputFormColor;
+            ibFiltered.Image = inputFormComplement;
+
+            ////Converting Emgu.CV.Image to Bitmap
+            ////The Image class has a ToBitmap() function that return a Bitmap object, which can easily be displayed on a PictureBox control using Windows Form.
+            //Image<Gray, Byte> imageInputFormComplement = inputFormComplement.ToImage<Gray, Byte>();
+            //Bitmap newImage = new Bitmap(ofdOpenFile.FileName);
+            //newImage = imageInputFormComplement.ToBitmap();
+
+
+
+            ////Create a 3 channel image of 400x200
+            ////A "Hello World" Example 
+            //using (Mat img = new Mat(200, 400, DepthType.Cv8U, 3))
+            //{
+            //    img.SetTo(new Bgr(255, 0, 0).MCvScalar); // set it to Blue color
+
+            //    //Draw "Hello, world." on the image using the specific font
+            //    CvInvoke.PutText(
+            //       img,
+            //       "Hello, world",
+            //       new System.Drawing.Point(10, 80),
+            //       FontFace.HersheyComplex,
+            //       1.0,
+            //       new Bgr(0, 255, 0).MCvScalar);
+
+            //    //Show the image using ImageViewer from Emgu.CV.UI
+            //    ImageViewer.Show(img, "Test Window");
+            //}
+        }
+    }
+}
